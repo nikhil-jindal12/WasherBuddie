36dc50a15a0ceb91711cd38e2e2d2b6a3b4ea3b0
jaydonfaal <jaydonfaal@gmail.com>
2024-10-23 18:01:34 -0400

    Created the UserEvent Class
    Made to the Design Document Specifications

---
+++ UserEvent.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+Public class UserEvent{
+    public String eventType;
+    public LocalDateTime date;
+    public String message;
+    public String hasNotification;
+}
