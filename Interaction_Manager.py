262e67f429584568edd83dcc345f04d4491abee0
Nikhil Jindal <njindal2004@gmail.com>
2024-10-26 14:09:21 -0400

    added comments and restructured methods for Notification class

--- Interaction_Manager.py
+++ Interaction_Manager.py
@@ -3,6 +3,12 @@ import User
 
 class Interaction_Manager:
     def add_washer(self):
+        """
+        Adds a new washing machine
+
+        Returns:
+            boolean: whether or not the washer was added successfully
+        """
         try:
             Machine('Washer')
             return True
@@ -10,6 +16,12 @@ class Interaction_Manager:
             return False
 
     def add_dryer(self):
+        """
+        Adds a new drying machine
+
+        Returns:
+            boolean: whether or not the dryer was added successfully
+        """
         try:
             Machine('Dryer')
             return True
@@ -17,11 +29,12 @@ class Interaction_Manager:
             return False
 
     def send_notification(self):
+        # engage another script that sends the user a text or email based on their notification preference
         return False
 
-    def add_user(self, user_name, notification_preference, user_phone_number, user_email, is_admin=False):
+    def add_user(self, user_name, notification_preference, user_phone_number, user_email, phone_carrier, is_admin=False):
         try:
-            User(user_name, notification_preference, user_phone_number, user_email, is_admin)
+            User(user_name, user_email, phone_carrier, notification_preference, user_phone_number, is_admin)
             # put an addition request into CRUD
             # will add line in DB with user
             return True
@@ -30,17 +43,25 @@ class Interaction_Manager:
 
     def remove_user(self, user_name):
         try:
-            # put a delete request into CRUD
+            # put a delete request into CRUD to search by the user's name
             # will search up in DB and delete that line
             return True
         except:
             return False
 
     def authenticate_log_in(self):
+        # send request to CRUD to validate that user's log in information
+        # database will need to be decrypted
         return False
 
     def get_white_list(self):
-        return False
+        try:
+            # put a request into CRUD to read all users
+            # will search up all valid users in the DB to return
+            return True
+        except:
+            return False
 
     def log_event(self):
+        # log any events with their timestamps into our database
         return False
