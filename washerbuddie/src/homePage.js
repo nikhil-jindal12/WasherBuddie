b647954566c2badaf09dd5c2e35909f3e02a9ee3
jaydonfaal <jaydonfaal@gmail.com>
2024-11-29 18:39:44 -0500

    ui merged into branchj

--- washerbuddie/src/homePage.js
+++ washerbuddie/src/homePage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from './Header';
-import Menu from './Menu';
+import Menu from './menu';
 import './App.css';
 
 function HomePage() {
